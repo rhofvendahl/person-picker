bc365747904725c5acbb14b9857300f3dc00ea92
Russell Hofvendahl <rhofvendahl@gmail.com>
2024-08-23 20:08:45 -0700

    feat: add people page bottom bar

--- src/components/people/PeoplePage.tsx
+++ src/components/people/PeoplePage.tsx
@@ -1,5 +1,14 @@
 const PeoplePage = () => {
-  return <div className="h-full">People page</div>;
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex-1">Pictures</div>
+      <div className="h-14 bg-gray-800 flex items-center justify-center gap-6">
+        <button className="rounded px-2 py-1 bg-gray-600">Button 1</button>
+        <button className="rounded px-2 py-1 bg-gray-600">Button 2</button>
+        <button className="rounded px-2 py-1 bg-gray-600">Button 3</button>
+      </div>
+    </div>
+  );
 };
 
 export default PeoplePage;
